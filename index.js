2b8590a3613fa7ce2b5a3fd324436d45f292332c
jocimar walter <jocimarwalter@bb.com.br>
2024-05-30 19:05:10 -0300

    atualizado

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const secureRoutes  = express.Router();
 import mongoose  from "mongoose";
 import dotenv from "dotenv"
 
+
+
 dotenv.config();
 app.use(express.static('public'))
 secureRoutes.use(express.static('public'))
 const basePath = '/notification-api';
+app.use(express.json());
+secureRoutes.use(express.json());
+
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
@@ -31,6 +37,16 @@ secureRoutes.get('/', function(req, res) {
     // Não é necessário renderizar nada, apenas enviar o arquivo HTML como resposta
     res.sendFile(__dirname +  '/public/index.html');
 });
+
+
+
+secureRoutes.post('/cadastrar', function(req, res) {
+
+    console.log(req.body)
+    
+    res.status(200).json({"retorno":"mensagem retorno"})
+});
+
 
 
 
