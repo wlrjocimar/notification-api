783bd718497bceefabf4a8034db49ae900d38311
jocimar walter <jocimarwalter@bb.com.br>
2024-05-30 13:32:29 -0300

    updated action

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import express from "express";
 const app = express();
 const router = express.Router();
+import mongoose  from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("DBConnection successfull!!")
+}).then(()=>{
+    console.log("If connection successful then i run")
+}).catch((error)=>{
+    console.log("Authenticated Failed!!", error)
+})
+
 
 
 
