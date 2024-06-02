e81c44ca952eeed1af29ac68fd252623d630b525
jocimar walter <jocimarwalter@bb.com.br>
2024-06-02 19:18:52 -0300

    atualizado

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const secureRoutes = express.Router();
 import path from "path";
 import { fileURLToPath } from 'url';
 import notificationRouter from "./src/routes/notifications.js";
+import  { Sender }  from 'node-gcm';
+// Chave do servidor para o GCM (substitua pela sua chave)
+const serverKey = 'dhfjkshfjhjhjfhsjfhss';
+const sender = new Sender(serverKey);
+import fs from 'fs';
+
+const serviceAccount = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf8'));
+
+import admin from 'firebase-admin'; // Certifique-se de ter o pacote 'firebase-admin' instalado
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+
 
 dotenv.config();
 const app = express();
@@ -44,6 +61,54 @@ secureRoutes.post('/cadastrar', function(req, res) {
         res.status(400).json({ "erro": "A solicitação deve ser enviada como JSON" });
     }
 });
+
+
+
+secureRoutes.post('/send-notification', async (req, res) => {
+    const { title, body } = req.body;
+  
+    if (!title || !body ) {
+      return res.status(400).send('Título, corpo da notificação e tokens dos dispositivos são obrigatórios');
+    }
+  
+    const messages = [{
+      notification: {
+        title,
+        body,
+      },
+      token:registeredDevices,
+    }];
+  
+    try {
+      const response = await admin.messaging().sendEach(messages);
+      console.log('Notificações enviadas com sucesso:', response);
+      res.status(200).json(response);
+    } catch (error) {
+      console.error('Erro ao enviar notificações:', error);
+      res.status(500).send('Erro ao enviar notificações');
+    }
+  });
+  
+
+
+const registeredDevices = ["ExponentPushToken[i1DVm1LVmIMkgKWrRoLiOx]"];
+
+// Rota para registrar dispositivos
+// Rota para registrar dispositivos , vamos usar os tokens do lado do cliente gerado com expo para regitrar
+secureRoutes.post('/register-device', (req, res) => {
+    const { expoPushToken } = req.body;
+    if (expoPushToken) {
+      // Armazena o expoPushToken em seu banco de dados ou em algum outro tipo de armazenamento
+      console.log(`Dispositivo registrado: ${expoPushToken}`);
+      res.status(200).send('Dispositivo registrado com sucesso');
+    } else {
+      res.status(400).send('Expo Push Token não fornecido');
+    }
+  });
+  
+
+
+
 
 secureRoutes.get('/test', function(req, res) {
     res.status(200).json({ "retorno": "resposta ok" });
---
+++ src/config/fcm.js
@@ -0,0 +1,2 @@
+import http2 from "http2";
+import dotent from "dotenv";
