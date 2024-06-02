2e488fd2b7bca15c976585913fd12935efb49f8e
jocimar walter <jocimarwalter@bb.com.br>
2024-06-02 10:40:31 -0300

    updated

--- index.js
+++ index.js
@@ -1,69 +1,51 @@
 import express from "express";
-const app = express();
-const secureRoutes  = express.Router();
-import mongoose  from "mongoose";
-import dotenv from "dotenv"
-import notificationRouter from "./src/routes/notifications.js"
-
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const secureRoutes = express.Router();
+import path from "path";
+import { fileURLToPath } from 'url';
+import notificationRouter from "./src/routes/notifications.js";
 
 dotenv.config();
-app.use(express.static('public'))
-secureRoutes.use(express.static('public'))
+const app = express();
+
+// To resolve __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const basePath = '/notification-api';
 app.use(express.json());
-secureRoutes.use(express.json());
 
-
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("DBConnection successfull!!")
-}).then(()=>{
-    console.log("If connection successful then i run")
-}).catch((error)=>{
-    console.log("Authenticated Failed!!", error)
-})
+    .then(() => {
+        console.log("DBConnection successful!!");
+    })
+    .then(() => {
+        console.log("If connection successful then I run");
+    })
+    .catch((error) => {
+        console.log("Authentication Failed!!", error);
+    });
 
-
-
-
-
-// Rota para servir o arquivo HTML
-secureRoutes.get('/', function(req, res) {
-    // Não é necessário renderizar nada, apenas enviar o arquivo HTML como resposta
-    res.sendFile(__dirname +  '/public/index.html');
+// Route to serve the HTML file
+secureRoutes.get('/swagger', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-
 
 secureRoutes.post('/cadastrar', function(req, res) {
-
-    console.log(req.body)
-    
-    res.status(200).json({"retorno":"mensagem retorno"})
+    console.log(req.body);
+    res.status(200).json({ "retorno": "mensagem retorno" });
 });
 
-
-
-app.use(basePath, secureRoutes);
 secureRoutes.get('/test', function(req, res) {
-
-    
-    
-    res.status(200).json({"retorno":"resposta ok"})
+    res.status(200).json({ "retorno": "resposta ok" });
 });
 
 
+app.use(basePath,secureRoutes);
 
-
-
-
-
-
-
-
-
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("Connected to backend");
-})
+});
